8854906c1741e4ad0250d6e223a5d1f40e8a2c14
Ruchita <ruchita@unlearner.com>
2015-10-12 20:49:37 -0700

    Update 01-multiples-of-3-and-5.js

--- 01-multiples-of-3-and-5.js
+++ 01-multiples-of-3-and-5.js
@@ -10,7 +10,7 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 var total = 0;
 for (var i=0; i < 1000; i++) {
 	if (i % 3 === 0 || i % 5 === 0) {
-		total = total + i;
+		total += i;
 	}
 }
 console.log(total);
